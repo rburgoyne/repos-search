0d894b2116b60477f11ac1cdf1ee638314493234
solsson <solsson@5a4de678-0f31-0410-84e4-bbaf904ce06c>
2009-09-05 07:42:13 +0000

    Added license headers to source files
    git-svn-id: https://labs.repos.se/data/search/trunk@608 5a4de678-0f31-0410-84e4-bbaf904ce06c

--- repos-search.load.js
+++ repos-search.load.js
@@ -1,5 +1,20 @@
 /*
- * Repos Search GUI (c) Staffan Olsson repossearch.com
+ * Repos Search GUI (c) 2009 Staffan Olsson repossearch.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Include in Repos Style header to get a search box in the menu bar.
  * Requires jQuery 1.3+
  * $LastChangedRevision$
