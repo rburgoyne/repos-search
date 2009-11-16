d338f1397b5adfb5d654944946269c4563a0eca8
solsson <solsson@5a4de678-0f31-0410-84e4-bbaf904ce06c>
2009-11-16 21:34:10 +0000

    Implemented the search proxy in PHP because it is more widely available on Apache than modpython
    git-svn-id: https://labs.repos.se/data/search/trunk@659 5a4de678-0f31-0410-84e4-bbaf904ce06c

---
+++ repos-search/index.php
@@ -0,0 +1,64 @@
+<?php 
+/** Search proxy
+
+   Copyright 2009 Staffan Olsson repossearch.com
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+Proxies searces to Solr
+$LastChangedRevision$
+ */
+
+// settings for accessing solr
+$solrhost = 'localhost';
+$solrport = 8080;
+$solrapp = '/solr/';
+$schema = 'svnhead/';
+// parameters for solr search
+$wt = 'json';
+$start = 0;
+$rows = 10;
+$indent = 'on';
+
+// forward to search page if there is no query
+if (!isset($_GET['q'])) {
+?>
+<html>
+<head>
+<title>Repos Search proxy: no query yet</title>
+<meta http-equiv="refresh" content="1;url=search.html" />
+</head>
+<body>
+<p>Redirecting to <a href="search.html">sample search form</a> becase <code>q</code> is not set.</p>
+</body>
+</html>
+<?php
+exit;
+}
+
+header('Content-Type: text/plain');
+// the search
+$query = 'q='.rawurlencode($_GET['q']);
+if (isset($_GET['base'])) {
+	$query .= '&base='.$_GET['base']; // may not contain non-URI characters
+}
+// search URI
+$url = "http://$solrhost:$solrport$solrapp$schema".'select/';
+// request parameters
+$url .= "?$query&wt=$wt&start=$start&rows=$rows&indent=$indent";
+
+$fp = fopen($url, 'r');
+fpassthru($fp);
+fclose($fp);
+
+?>
